00de0d9382e91e64ed7d714d74452eaf8dedd531
sunnielens <54705069+lenhatquang97@users.noreply.github.com>
2026-08-17 14:15:45 +0700

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.java
@@ -0,0 +1,52 @@
+class Solution {
+    ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
+    ArrayList<Integer> inDegreeLs = new ArrayList<>();
+    public void addEdges(int[][] prerequisites) {
+        for (int i = 0; i < prerequisites.length; i++) {
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+            adjList.get(u).add(v);
+        }
+    }
+    public void initCourses(int n) {
+        for (int i = 0; i < n; i++) {
+            adjList.add(new ArrayList<>());
+            inDegreeLs.add(0);
+        }
+        
+    }
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        Queue<Integer> q = new LinkedList<>();
+
+        initCourses(numCourses);
+        addEdges(prerequisites);
+
+        for (int i = 0; i < numCourses; i++) {
+            for (int next : adjList.get(i)) {
+                inDegreeLs.set(next, inDegreeLs.get(next) + 1);
+            }
+        }
+
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegreeLs.get(i) == 0) {
+                q.add(i);
+            }
+        }
+
+        while (!q.isEmpty()) {
+            Integer tmp = q.poll();
+            for (int next : adjList.get(tmp)) {
+                inDegreeLs.set(next, inDegreeLs.get(next) - 1);
+                if (inDegreeLs.get(next) == 0) {
+                    q.add(next);
+                }
+            }
+        }
+
+        for (int i = 0; i < inDegreeLs.size(); i++) {
+            if (inDegreeLs.get(i) < 0 || inDegreeLs.get(i) > 0) return false;
+        }
+
+        return true;
+    }
+}
