18f685789235dde1b7f0393f5b138d122105caf9
sunnielens <54705069+lenhatquang97@users.noreply.github.com>
2026-08-19 16:18:06 +0700

    Add: insert-new-interval - submission-0

---
+++ Data Structures & Algorithms/insert-new-interval/submission-0.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[][] convertTo2dArray(List<int[]> inp) {
+        int[][] res = new int[inp.size()][2];
+        for (int i = 0; i < inp.size(); i++) {
+            res[i][0] = inp.get(i)[0];
+            res[i][1] = inp.get(i)[1];
+        }
+        return res;
+    }
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> res = new ArrayList<>();
+        for (int i = 0; i < intervals.length; i++) {
+            if (newInterval[1] < intervals[i][0]) {
+                res.add(newInterval);
+                for (int j = i; j < intervals.length; j++) {
+                    res.add(intervals[j]);
+                }
+                return convertTo2dArray(res);
+            } else if (newInterval[0] > intervals[i][1]) {
+                res.add(intervals[i]);
+            } else {
+                newInterval = new int[]{
+                    Math.min(newInterval[0], intervals[i][0]),
+                    Math.max(newInterval[1], intervals[i][1])
+                };
+            }
+        }
+        res.add(newInterval);
+        return convertTo2dArray(res);
+    }
+}
